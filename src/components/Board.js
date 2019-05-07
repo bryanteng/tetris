c336cf621e2541e72528a1ca966e090a76d17dbb
Bryan Teng <bteng1226@gmail.com>
2019-05-07 09:38:37 -0500

    modular functions and error fixes

--- src/components/Board.js
+++ src/components/Board.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import Tetriminoes from './Tetriminoes'
 import '../styles/Board.css'
 import { connect } from 'react-redux'
-import { getPiece } from '../actions/pieces'
+import { getPiece, fillQueue } from '../actions/pieces'
 
 class Board extends Component {
 
@@ -15,56 +15,86 @@ class Board extends Component {
       handleOnKeyDown=(event) => {
         let pressed = event.key
         if(pressed === "ArrowRight"){
-            let x = this.state.x_coord
-            if(x < 10) this.setState({x_coord: x +1}, this.draw(x, this.state.y_coord))
+            this.move(1)
         }else if(pressed ==="ArrowLeft"){
-            let x = this.state.x_coord
-            if(x > 0) this.setState({x_coord: x -1}, this.draw(x, this.state.y_coord))
+            this.move(-1)
         }else if(pressed ==="ArrowUp"){
             console.log("up")
         }else if(pressed === "ArrowDown"){
-            console.log("down")
-            this.setState({y_coord: this.state.y_coord +1}, this.draw(this.state.x_coord, this.state.y_coord))
+            this.drop()
         }
       }
 
-      gameStart =()=>{
+      gameStart = () => {
         this.props.getPiece()
-        this.draw(this.state.x_coord,this.state.y_coord)
-        if(this.props.currentPiece[0]){
-        // setInterval(()=> {
-        //     this.setState({backgroundColor: ret[counter]})
-        //     counter++},500)
-        }
+        // if(this.props.currentPiece) {
+        //     this.draw(4,0)
+        // }
       }
 
-    draw = (x_coord,y_coord)=>{  
+    draw = (x_coord,y_coord) => {  
         let board = this.state.boardState
-    //   if(this.props.currentPiece[0]) console.log(this.props.currentPiece[0][0], "props")
-    //   if(!!(board[0][4] && board[0][5] && board[0][6]) === false){
-          for(let blocks in this.props.currentPiece){
-              for(let block in this.props.currentPiece){
-              let temp = +block + +x_coord
-              board[+blocks + y_coord][+temp] = this.props.currentPiece[blocks][block]
-              }
-        //   }
-      }
-      this.setState({boardState: board}, console.log(board, "board after key"))
+        for(let blocks in this.props.currentPiece){
+            for(let block in this.props.currentPiece){
+                let temp = +block + +x_coord
+                board[+blocks + y_coord][+temp] = this.props.currentPiece[blocks][block]
+            }
+        }
     }
 
+    drop = () => {
+        this.setState({y_coord: this.state.y_coord+1})
+        if(!this.collide()) 
+            this.draw(this.state.x_coord,this.state.y_coord)
+    }
+    
+    move = (offset) => {
+        this.setState({x_coord: this.state.x_coord+offset})
+        if (this.collide()) {
+            this.setState({x_coord: this.state.x_coord - offset})
+        }else{
+            this.draw(this.state.x_coord,this.state.y_coord)
+        }
+    }
+    collide = () => {
+        let m = this.state.boardState
+        for (let y = 0; y < m.length; ++y) {
+            for (let x = 0; x < m[y].length; ++x) {
+                if (m[y][x] !== 0 &&
+                   (m[y + this.state.y_coord] &&
+                    m[y + this.state.y_coord][x + this.state.x_coord]) !== 0) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    // componentDidUpdate(prevProps, prevState) {
+    //     if(this.props.currentPiece !== prevProps.currentPiece && this.state.boardState !== prevState.boardState)
+    //     this.draw(this.state.x_coord,this.state.y_coord)   
+    // }
+    
+    componentDidMount() {
+        this.props.fillQueue()
+    }
+    
+
     render() {
+        console.log(this.props.currentPiece)
         return (
             <div className="BoardC">
                 <Tetriminoes/>
-                <table className="Board" onKeyDown={this.handleOnKeyDown} tabIndex="0">
-                    {this.state.boardState.map((line,index) =>{
-                    return <tr className="line">{line.map((block,index)=>{
-                            return <td className="block" style={{backgroundColor: block ? block : null}} > </td>
-                        })}</tr>
-                    }) }
+                <table className="Board" focus="true" onKeyDown={this.handleOnKeyDown} tabIndex="0">
+                    <tbody>
+                        {this.state.boardState.map((line,vertical_index) =>{
+                        return <tr className="line" key={vertical_index}>{line.map((block,horizontal_index)=>{
+                                return <td className="block" key={`${vertical_index}${horizontal_index}`} style={{backgroundColor: block ? block : null}} > </td>
+                            })}</tr>
+                        }) }
+                    </tbody>
                 </table>
                 <button onClick={this.gameStart} >start </button>
-                <button onClick={this.handleButtonPress}>push me for a piece</button>
             </div>
         );
     }
@@ -72,11 +102,11 @@ class Board extends Component {
 
 const mapStateToProps = (state) =>{
     return{
-      currentPiece: state.pieces.currentPiece,
+      currentPiece: state.pieces.currentPiece
     }
   }
   
-export default connect(mapStateToProps, {getPiece})(Board)
+export default connect(mapStateToProps, {getPiece, fillQueue})(Board)
 
     //   handleButtonPress =()=>{
     //     let board = this.state.boardState
@@ -92,3 +122,10 @@ export default connect(mapStateToProps, {getPiece})(Board)
     //     }
     //     this.setState({boardState: board})
     //   }
+
+
+        // if(this.props.currentPiece[0]){
+        // setInterval(()=> {
+        //     this.setState({backgroundColor: ret[counter]})
+        //     counter++},500)
+        // }
--- src/components/PieceDisplay.js
+++ src/components/PieceDisplay.js
@@ -4,14 +4,12 @@ import { fillQueue } from '../actions/pieces'
 
 class PieceDisplay extends Component {
 
-    componentDidMount(){
-        this.props.fillQueue() 
-    }
+
 
     render() {
         return (
         <div className="Pieces">
-            {this.props.piecesQueue.map(pieces=><div className="Piece">{pieces.map(piece=> <tr>{piece.map(block=><td className="block" style={{backgroundColor: block ? block : null}}></td>)}</tr> ) }</div>)}
+            {this.props.piecesQueue.map((pieces,key)=><div key={key} className="Piece">{pieces.map((piece,index)=> <tr key={index}>{piece.map((block,index2)=><td className="block" key={`${index}${index2}`} style={{backgroundColor: block ? block : null}}></td>)}</tr> ) }</div>)}
         </div>
         )
     }
